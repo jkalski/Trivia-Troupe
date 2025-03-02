e5c0ff3f8a355e2ace12bdba6416b0f7634b789a
SwegCet <ethantruong0804@gmail.com>
2025-03-02 00:58:39 -0800

    Just making sure buttons work

---
+++ src/scripts/category.js
@@ -0,0 +1,5 @@
+let categorySelectBtn = document.getElementsByClassName("category-select-btn")[0]; // getElementsByClassName returns an array
+
+categorySelectBtn.addEventListener("click", function(){
+    console.log("I was clicked");
+});
