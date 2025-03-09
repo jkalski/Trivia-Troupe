15e10ed4d69dd3467dcbd133af4040aafda06e97
SwegCet <ethantruong0804@gmail.com>
2025-03-08 20:20:46 -0800

    Changed to ensure Login Form is hidden

--- src/scripts/loginPage.js
+++ src/scripts/loginPage.js
@@ -1,12 +1,14 @@
 let signUp = document.getElementById("create-account-button");
 let modalContainer = document.getElementById("modalContainer");
 let xButton = document.getElementById("x-btn");
-
+let loginContainer = document.getElementById("login-form-wrapper");
 
 signUp.addEventListener("click", function() {
     modalContainer.classList.add('show');
+    loginContainer.classList.add('hidden')
 });
 
 xButton.addEventListener("click", function() {
     modalContainer.classList.remove('show');
+    loginContainer.classList.remove('hidden');
 });
